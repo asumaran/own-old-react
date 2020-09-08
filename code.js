e9d33e83a71638e2e1ef63a1e55cf5eda951f474
Alfredo Sumaran <a.sumaran@gmail.com>
2020-09-07 21:58:37 -0500

    Ability to remove old props

--- code.js
+++ code.js
@@ -36,42 +36,90 @@ function createTextElement(value) {
   };
 }
 
-function render(element, parentDom) {
+let rootInstance = null;
+
+function render(element, container) {
+  prevInstance = rootInstance;
+  const nextInstance = reconcile(container, prevInstance, element);
+  console.log(nextInstance);
+  rootInstance = nextInstance;
+}
+
+function reconcile(parentDom, instance, element) {
+  const newInstance = instantiate(element);
+  parentDom.appendChild(newInstance.dom);
+  return newInstance;
+}
+
+function instantiate(element) {
   const { type, props } = element;
 
   let dom;
-
-  if (type === "TEXT ELEMENT") {
+  if (type === TEXT_ELEMENT) {
     dom = document.createTextNode("");
   } else {
     dom = document.createElement(type);
   }
 
-  // Set event listeners
-  const eventListeners = Object.keys(props).filter(propsIsEventListener);
-  eventListeners.forEach((eventName) => {
+  updateDomProperties(dom, {}, props);
+
+  const childElements = props.children || [];
+  const childInstances = childElements.map(instantiate);
+
+  childInstances.forEach((instance) => {
+    dom.appendChild(instance.dom);
+  });
+
+  return {
+    element,
+    dom,
+    childInstances,
+  };
+}
+
+function updateDomProperties(dom, prevProps, nextProps) {
+  Object.keys(prevProps);
+  // Remove properties
+  const prevOnlyProps = Object.keys(prevProps).filter(isRegularProp);
+  prevOnlyProps.forEach(() => {
+    dom[propName] = null;
+  });
+
+  // Remove event listeners
+  const prevEventListeners = Object.keys(prevProps).filter(
+    propsIsEventListener
+  );
+  prevEventListeners.forEach((eventName) => {
     const eventType = eventName.toLowerCase().substring(2);
-    dom.addEventListener(eventType, props[eventName]);
+    dom.removeEventListener(eventType, prevProps[name]);
   });
 
   // Set properties
-  const onlyProps = Object.keys(props).filter(isRegularProp);
+  const onlyProps = Object.keys(nextProps).filter(isRegularProp);
   onlyProps.forEach((propName) => {
-    dom[propName] = props[propName];
+    dom[propName] = nextProps[propName];
   });
 
-  // Render children
-  (props.children || []).forEach((element) => {
-    render(element, dom);
+  // Set event listeners
+  const eventListeners = Object.keys(nextProps).filter(propsIsEventListener);
+  eventListeners.forEach((eventName) => {
+    const eventType = eventName.toLowerCase().substring(2);
+    dom.addEventListener(eventType, nextProps[eventName]);
   });
-
-  parentDom.appendChild(dom);
 }
 
 // App
-const root = document.getElementById("root");
-const element = (
-  <span onClick={() => console.log("Text clicked!")}>Hola mundo!</span>
-);
+function tick() {
+  const root = document.getElementById("root");
+  const time = new Date().toISOString();
+  const element = (
+    <span onClick={() => console.log("clicked!")}>Hola mundo</span>
+  );
+  render(element, root);
+}
 
-render(element, root);
+tick();
+
+setTimeout(() => {
+  tick();
+}, 1000);
